220428cc6d4669a20151446b98980150070b9379
Lionel Desorme <lionel.desorme@free.fr>
2018-06-21 12:25:37 +0200

    First test commit

---
+++ Runner/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Runner
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+
+
+            //Client
+            var client = new NamedPipeClientStream($"PipesOfPiece_{Process.GetCurrentProcess().Id}");
+            client.Connect();
+            StreamWriter writer = new StreamWriter(client);
+
+            try
+            {
+                Console.WriteLine("Start Runner");
+
+
+
+
+
+                foreach (var arg in args)
+                {
+                    Console.WriteLine(arg);
+                }
+                if (args.Length >= 1 && args[0] == "ex")
+                {
+                    Thread.Sleep(2000);
+                    throw new Exception("error");
+                }            // Do some calc
+                Thread.Sleep(10000);
+                Console.WriteLine("Calc finished");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                writer.WriteLine(e.Message);
+                writer.Flush();
+                return 1;
+            }
+
+            writer.WriteLine("OK");
+            writer.Flush();
+            return 0;
+        }
+    }
+}
---
+++ TestChild/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestChild
+{
+    class Program
+    {
+
+        /*
+         * static void StartServer()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                var server = new NamedPipeServerStream("PipesOfPiece");
+                server.WaitForConnection();
+                StreamReader reader = new StreamReader(server);
+                StreamWriter writer = new StreamWriter(server);
+                while (true)
+                {
+                    var line = reader.ReadLine();
+                    writer.WriteLine(String.Join("", line.Reverse()));
+                    writer.Flush();
+                }
+            });
+        }*/
+
+
+
+
+
+        static void LaunchRunner(string arg)
+        {
+            Console.WriteLine("Process0");
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "Runner.exe";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = arg;
+
+
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using-statement will close.
+                Console.WriteLine("Process");
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    var server = new NamedPipeServerStream($"PipesOfPiece_{exeProcess.Id}");
+                    server.WaitForConnection();
+                    StreamReader reader = new StreamReader(server);
+                    Console.WriteLine("Connected");
+
+                    while (!exeProcess.WaitForExit(1000))
+                    {
+                        Console.WriteLine(exeProcess.Id);
+                        Console.WriteLine("Start readline");
+                        var line = reader.ReadLine();
+                        Console.WriteLine($"line={line}");
+                    }
+                    Console.WriteLine($"Process finished {exeProcess.Id} : {exeProcess.ExitCode}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void WaitTask(Task t)
+        {
+            while (!t.IsCompleted)
+                Thread.Sleep(1000);
+            Console.WriteLine($"Final status {t.Status}");
+        }
+
+        static void Start()
+        {
+            Task t1 = new Task( () => LaunchRunner("ex"));
+            t1.Start();
+            Task t2 = new Task( () => LaunchRunner("test"));
+            t2.Start();
+            Task.WaitAll(t1, t2);
+            Console.WriteLine("Tasks finished");
+        }
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello world");
+
+                Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
